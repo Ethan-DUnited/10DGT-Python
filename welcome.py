b9bc0d5636cdb3c4c3e54fe97815759b7bd4d61a
Ethan-DUnited <168596253+Ethan-DUnited@users.noreply.github.com>
2024-05-01 10:15:14 +1200

    Add files via upload
    My first program:
    - Prints hello world

---
+++ welcome.py
@@ -0,0 +1,8 @@
+# Hello World Program
+# Author: Ethan du Toit
+# Date: 12th April 2024
+# Version: 1.0
+
+# Hello World Program
+print ("Hello World!")
+print ("Man United about to win the FA Cup")
