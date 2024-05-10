9438e05a7bb06ec5dfe41c4ec92fb02111c97a0d
Ethan-DUnited <168596253+Ethan-DUnited@users.noreply.github.com>
2024-05-10 14:33:44 +1200

    Variable Basics
    Learning about variable types (strings, numbers, boolean) & common mistakes with variables.

---
+++ P_02_variables.py
@@ -0,0 +1,30 @@
+# Code to learn about variables
+# Author: Ethan du Toit
+# Date: 8 May 2024
+# Version: V1
+
+# Variables can be a string (note speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5" # Speech marks make Python think something is text
+
+# Variables can be Boolean (True / False)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print (love_programming)
+
+# This variable has a problem:
+my_name = "Ethan"
+
+
+print(fav_num * 2)
+print(num_string * 5) 
+
+# Constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
